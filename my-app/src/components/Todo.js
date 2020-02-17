bf1148a4433bed2e242fd30cdbaee8403c3c5237
alexis villaraza <alexis.borja@gmail.com>
2020-02-17 13:19:58 -0700

    added input field

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -1,14 +1,23 @@
-import React, { useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
 import { todoReducer, initialState } from '../reducers/reducer';
 
 const Todo = () => {
     const [state, dispatch] = useReducer(todoReducer, initialState);
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleChanges = e => {
+        setNewTodo(e.target.value);
+    };
 
     return (
         <div>
             <h1>
                 {state.item}
             </h1>
+       
+            <input type='text' name='newTodo' value={newTodo} onChange={handleChanges} />
+            
         </div>
     )
 }
