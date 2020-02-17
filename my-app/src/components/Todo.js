4c9027ae3c44d2ce923d4e3d20ac91e8d6b59cde
alexis villaraza <alexis.borja@gmail.com>
2020-02-17 13:45:03 -0700

    able to add todo item

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -13,10 +13,18 @@ const Todo = () => {
     return (
         <div>
             <h1>
-                {state.item}
+                {state.item.map(i => {
+                    return <p>{i}</p>
+                })}
             </h1>
        
             <input type='text' name='newTodo' value={newTodo} onChange={handleChanges} />
+
+            <button onClick={() => {
+                dispatch({ type: 'UPDATE_TODO', payload: newTodo });
+            }}>
+                Add Todo Item
+            </button>
             
         </div>
     )
--- my-app/src/reducers/reducer.js
+++ my-app/src/reducers/reducer.js
@@ -1,10 +1,23 @@
 export const initialState = {
-    item: 'Learn about reducers',
+    editing: false,
+    item: ['Learn about reducers'],
     completed: false,
     id: 3892987589
       
 }
 
-export const todoReducer = () => {
-    return 
+export const todoReducer = (state, action) => {
+    console.log(action);
+    switch (action.type) {
+       
+        case 'UPDATE_TODO':
+            return {
+                ...state,
+                item: [...state.item, action.payload],
+                editing: false,
+                id: newDate()
+            };
+            default:    
+            return state; 
+    }
 }
